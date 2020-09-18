cd7bfb71aae18bb8a8ae997b9264c8bd938a2ab0
Natalia Lemańczyk <nataliaolga2@gmail.com>
2020-09-18 18:56:13 +0200

    Add class active to the correct article in js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   event.preventDefault();
 
@@ -29,16 +29,22 @@ for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
   const articleSelector = clickedElement.getAttribute('href');
 
   console.log(articleSelector);
 
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
-  /* add class 'active' to the correct article */
+  const targetArticle = document.querySelector(articleSelector);
+
+  console.log(targetArticle);
+
+  /* [DONE] add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
