db10c4d81f18cc859cc6498533bd3d94a1d9f311
Natalia Lemańczyk <nataliaolga2@gmail.com>
2020-09-21 16:33:06 +0200

    Create loop for each article and find their id attribute

--- js/script.js
+++ js/script.js
@@ -52,3 +52,50 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+
+  function clearMessages(){
+	   titleList.innerHTML = '';
+}
+
+   clearMessages();
+  /* for each article */
+
+  const articles = document.querySelectorAll (optArticleSelector);
+
+  for (let article of articles) {
+    console.log(article);
+
+    /* get the article id */
+
+    const articleId = article.getAttribute('id');
+
+    console.log(articleId);
+
+    /* find the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* get the title from the title element */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+    console.log (linkHTML);
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+}
+
+generateTitleLinks();
