256b77e4223165af421c9e7e4d1525fb0face601
Natalia Lemańczyk <nataliaolga2@gmail.com>
2020-09-22 15:27:46 +0200

    generate html of the taglinks

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ const titleClickHandler = function(){
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(){
@@ -115,27 +115,51 @@ for(let link of links){
 function generateTags(){
   /* find all articles */
 
+  const articles = document.querySelectorAll (optArticleSelector);
+
   /* START LOOP: for every article: */
+
+  for (let article of articles) {
+    console.log(article);
 
     /* find tags wrapper */
 
+    const titleList = article.querySelector(optArticleTagsSelector);
+
     /* make html variable with empty string */
 
+    let html = '';
+
     /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+
+    console.log(articleTags);
 
     /* split tags into array */
 
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
+
     /* START LOOP: for each tag */
+    for(let tag of articleTagsArray){
+      console.log(tag);
 
       /* generate HTML of the link */
 
+      const linkHTML = '<li><a href="#tag-' + tag +'">' + tag + '</a></li>';
+      console.log(linkHTML);
+
       /* add generated code to html variable */
 
-    /* END LOOP: for each tag */
+      html = html + linkHTML;
 
+    /* END LOOP: for each tag */
+    }
+    titleList.innerHTML = html;
     /* insert HTML of all the links into the tags wrapper */
 
   /* END LOOP: for every article: */
+  }
 }
 
 generateTags();
